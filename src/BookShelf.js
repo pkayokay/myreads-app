dab6338dba758ef284991dd3e8aa592d85394fd6
Paul Kim <pkim.dev@gmail.com>
2017-09-06 19:54:14 -0400

    Rename bookshelf map to book

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -8,13 +8,13 @@ class BookShelf extends React.Component {
         <h2 className="bookshelf-title">{this.props.sectionTitle}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-          {this.props.books.map(n => (
-            <li key={n.title}>
+          {this.props.books.map(book => (
+            <li key={book.title}>
               <div className="book">
                 <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${n.bookURL})`}}></div>
+                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.bookURL})`}}></div>
                   <div className="book-shelf-changer">
-                    <select value={this.props.value} onChange={this.props.handleChange}>
+                    <select value={book.shelf}>
                       <option value="none" disabled>Move to...</option>
                       <option value="currentlyReading">Currently Reading</option>
                       <option value="wantToRead">Want to Read</option>
@@ -23,8 +23,8 @@ class BookShelf extends React.Component {
                     </select>
                   </div>
                 </div>
-                <div className="book-title">{n.title}</div>
-                <div className="book-authors">{n.author}</div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.author}</div>
               </div>
             </li>
             ))}
